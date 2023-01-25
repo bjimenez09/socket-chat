0de69e9860d139f5d1759521baa2d3e19be6fb42
Brandon Jimenez <bjimenezp09@gmail.com>
2023-01-25 05:24:58 -0600

    added heroku link and deleted files

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./components/Chat/Chat";
 
-const socket = io.connect("http://localhost:3001");
+const socket = io.connect("https://ghostsocket-chat.herokuapp.com/");
 
 function App() {
   const [username, setUsername] = useState("");
